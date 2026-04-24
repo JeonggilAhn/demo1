59f170005423d25a6d2a1bc34455b6887fa5e80e
jeonggil <jeonggilahn@gmail.com>
2026-04-24 13:22:46 +0900

    A class 개발 완료

---
+++ src/main/java/com/example/demo1/ATest.java
@@ -0,0 +1,9 @@
+package com.example.demo1;
+
+public class ATest {
+    public static void main(String[] args) {
+        Demo1Application dm = new Demo1Application();
+
+        dm.getA();
+    }
+}
